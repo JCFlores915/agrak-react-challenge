d51b9a388b780d435d23faf1866b21706e4e84f7
JCFlores915 <juanflores915@yahoo.es>
2023-02-08 01:54:58 -0600

    change header

--- src/issues/components/Header.tsx
+++ src/issues/components/Header.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
     return (
         <nav className="navbar bg-dark color-white">
             <div className="container-fluid">
-                <span className="navbar-brand mb-0 h1 text-white">Agrak React Challenge</span>
+                <span className="navbar-brand mb-0 h1 text-white">Agrak React Challenge - Juan Carlos Flores Gutierrez</span>
 
             </div>
         </nav>
